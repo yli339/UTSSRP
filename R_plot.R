10e4b55c824d6f45cd9e8a0d85736cb7db6f5f72
yli339 <liyichen121can@163.com>
2025-06-12 16:24:24 -0400

    Add files via upload

--- R_plot.R
+++ R_plot.R
@@ -5,7 +5,7 @@ df <- as.data.frame(df_complete)
 colnames(df) <- c("Laureate", "Title", "Prize_Winning", "P_Value", "Gender", 
                   "Field", "Citations_Count", "Citation_Percentile", "Nobel_Citation_Percentile", "Publication_Year")
 
-# Divide by field and P_value
+# Divide by field and Relative Position
 chem_p <- df %>% filter(Field == "Chemistry", Prize_Winning == "YES") %>% 
   mutate(P_bin = cut(P_Value, breaks = seq(0, 1, by = 0.1), include.lowest = TRUE))
 phys_p <- df %>% filter(Field == "Physics", Prize_Winning == "YES") %>%
@@ -18,30 +18,38 @@ combine_p <- df %>% filter(Prize_Winning == "YES") %>%
 
 
 
-# Histogram binned by P_value (For all papers)
+# Histogram binned by Relative Position (For Nobel-Prize papers)
 chem_p_plot <- ggplot(chem_p, aes(x = Citation_Percentile)) +
   geom_histogram(binwidth = 0.05, fill = "blue", color = "black") +
   facet_wrap(~P_bin, ncol = 5, scales = "free_y") +
-  labs(title = "Citation Percentile Distribution by P_Value Bins(Chemistry)",
-       x = "Citation Percentile", y = "Count of Papers") +
+  labs(title = "Citation Percentile Distribution binned by Relative Position(Chemistry)",
+       x = "Citation Percentile", y = "Count of Nobel-Prize Papers") +
   theme_minimal()
 chem_p_plot
 
 phys_p_plot <- ggplot(phys_p, aes(x = Citation_Percentile)) +
   geom_histogram(binwidth = 0.05, fill = "blue", color = "black") +
   facet_wrap(~P_bin, ncol = 5, scales = "free_y") +
-  labs(title = "Citation Percentile Distribution by P_Value Bins(Physics)",
-       x = "Citation Percentile", y = "Count of Papers") +
+  labs(title = "Citation Percentile Distribution binned by Relative Position(Physics)",
+       x = "Citation Percentile", y = "Count of Nobel-Prize Papers") +
   theme_minimal()
 phys_p_plot
 
 med_p_plot <- ggplot(med_p, aes(x = Citation_Percentile)) +
   geom_histogram(binwidth = 0.05, fill = "blue", color = "black") +
   facet_wrap(~P_bin, ncol = 5, scales = "free_y") +
-  labs(title = "Citation Percentile Distribution by P_Value Bins(Medicine)",
-       x = "Citation Percentile", y = "Count of Papers") +
+  labs(title = "Citation Percentile Distribution binned by Relative Position(Medicine)",
+       x = "Citation Percentile", y = "Count of Nobel-Prize Papers") +
   theme_minimal()
 med_p_plot
+
+combine_p_plot <- ggplot(combine_p, aes(x = Citation_Percentile)) +
+  geom_histogram(binwidth = 0.05, fill = "blue", color = "black") +
+  facet_wrap(~P_bin, ncol = 5, scales = "free_y") +
+  labs(title = "Citation Percentile Distribution binned by Relative Position",
+       x = "Citation Percentile", y = "Count of Nobel-Prize Papers") +
+  theme_minimal()
+combine_p_plot
 
 
 
@@ -95,26 +103,26 @@ med_pl_plot
 # check for a single laureate
 chem_st <- chem_p %>% filter(Laureate == "stoddart, j")
 st_plot <- chem_st %>%
-  ggplot(aes(x = Citation_Percentile)) +
-  geom_histogram(binwidth = 0.05, fill = "red", color = "black") +
+  ggplot(aes(x = Citations_Count)) +
+  geom_histogram(binwidth = 50, fill = "red", color = "black") +
   geom_vline(data = chem_st, 
              aes(xintercept = Nobel_Citation_Percentile),
              color = "blue", linetype = "dashed", linewidth = 0.5) +
-  labs(title = "Citation Percentile Distribution by Laureates(st)",
-       x = "Citation Percentile", y = "Count of Papers") +
+  labs(title = "Citation Counts Distribution by Laureates(stoddart, j)",
+       x = "Citation Counts", y = "Count of Papers") +
   theme_minimal()
 st_plot
 
 
 
-# Linear Regression across fields (only prize-winning papers)
+# Linear Regression across fields (for Nobel-Prize papers)
 data_chem <- combine_p %>% filter(Field == "Chemistry")
 
 lm_chem_plot <- ggplot(data_chem, aes(x = P_Value, y = Citation_Percentile)) +
   geom_point(color = "red") +
   geom_smooth(method = "lm", color = "blue", se = FALSE) +
-  labs(title = "Linear Regression: Citation Percentile vs P_Value (Chemistry)",
-       x = "P_Value", y = "Citation Percentile") +
+  labs(title = "Linear Regression: Citation Percentile vs Relative Position (Chemistry)",
+       x = "Relative", y = "Citation Percentile") +
   theme_minimal()
 lm_chem_plot
 
@@ -126,7 +134,7 @@ data_phys <- combine_p %>% filter(Field == "Physics")
 lm_phys_plot <- ggplot(data_phys, aes(x = P_Value, y = Citation_Percentile)) +
   geom_point(color = "red") +
   geom_smooth(method = "lm", color = "blue", se = FALSE) +
-  labs(title = "Linear Regression: Citation Percentile vs P_Value (Physics)",
+  labs(title = "Linear Regression: Citation Percentile vs Relative Position (Physics)",
        x = "P_Value", y = "Citation Percentile") +
   theme_minimal()
 lm_phys_plot
@@ -139,13 +147,21 @@ data_med <- combine_p %>% filter(Field == "Medicine")
 lm_med_plot <- ggplot(data_med, aes(x = P_Value, y = Citation_Percentile)) +
   geom_point(color = "red") +
   geom_smooth(method = "lm", color = "blue", se = FALSE) +
-  labs(title = "Linear Regression: Citation Percentile vs P_Value (Medicine)",
+  labs(title = "Linear Regression: Citation Percentile vs Relative Position (Medicine)",
        x = "P_Value", y = "Citation Percentile") +
   theme_minimal()
 lm_med_plot
 
 med_model <- lm(Citation_Percentile ~ P_Value, data = data_med)
 summary(med_model)
+
+lm_combine_plot <- ggplot(combine_p, aes(x = P_Value, y = Citation_Percentile)) +
+  geom_point(color = "red") +
+  geom_smooth(method = "lm", color = "blue", se = FALSE) +
+  labs(title = "Linear Regression: Citation Percentile vs Relative Position",
+       x = "Relative Position", y = "Citation Percentile") +
+  theme_minimal()
+lm_combine_plot
 
 
 
@@ -165,36 +181,56 @@ summary(overall_model_1)
 decade_plot_ci <- ggplot(combine_d, aes(x = Citation_Percentile)) +
   geom_histogram(binwidth = 0.05, fill = "blue", color = "black") +
   facet_wrap(~decade_bin, ncol = 5, scales = "free_y") +
-  labs(title = "Citation Percentile Distribution by decade Bins",
-       x = "Citation Percentile", y = "Count of Papers") +
+  labs(title = "Citation Percentile Distribution binned by Decade",
+       x = "Citation Percentile", y = "Count of Nobel-Prize Papers") +
   theme_minimal()
 decade_plot_ci
 
 decade_plot_p <- ggplot(combine_d, aes(x = P_Value)) +
   geom_histogram(binwidth = 0.05, fill = "blue", color = "black") +
   facet_wrap(~decade_bin, ncol = 5, scales = "free_y") +
-  labs(title = "P_Value Distribution by decade Bins",
-       x = "P_Value", y = "Count of Papers") +
+  labs(title = "Relative Position Distribution binned by Decade",
+       x = "Relative Position", y = "Count of Nobel_Prize Papers") +
   theme_minimal()
 decade_plot_p
+
+decade_combine_p <- ggplot(combine_d, aes(x = P_Value, y = Citation_Percentile)) +
+  geom_point() +
+  geom_smooth(method = "lm", color = "blue", se = FALSE) +
+  facet_wrap(~decade_bin, ncol = 5, scales = "free_y") +
+  labs(title = "Relative Position vs Citation Percentile binned by Decade",
+       x = "Relative Position", y = "Citation Percentile") +
+  theme_minimal()
+decade_combine_p
 
 
 # Binned by Gender
 gender_plot_ci <- ggplot(combine_d, aes(x = Citation_Percentile)) +
   geom_histogram(binwidth = 0.05, fill = "blue", color = "black") +
   facet_wrap(~Gender, ncol = 5, scales = "free_y") +
-  labs(title = "Citation Percentile Distribution by Gender",
-       x = "Citation Percentile", y = "Count of Papers") +
+  labs(title = "Citation Percentile Distribution binned by Gender",
+       x = "Citation Percentile", y = "Count of Nobel-Prize Papers") +
   theme_minimal()
 gender_plot_ci
 
 gender_plot_p <- ggplot(combine_d, aes(x = P_Value)) +
   geom_histogram(binwidth = 0.05, fill = "blue", color = "black") +
   facet_wrap(~Gender, ncol = 5, scales = "free_y") +
-  labs(title = "P_Value Distribution by Gender",
-       x = "P_Value", y = "Count of Papers") +
+  labs(title = "Relative Position Distribution binned by Gender",
+       x = "Relative Position", y = "Count of Nobel-Prize Papers") +
   theme_minimal()
 gender_plot_p
+
+
+
+gender_combine_p <- ggplot(combine_d, aes(x = P_Value, y = Citation_Percentile)) +
+  geom_point() +
+  geom_smooth(method = "lm", color = "blue", se = FALSE) +
+  facet_wrap(~Gender, ncol = 5, scales = "free_y") +
+  labs(title = "Relative Position vs Citation Percentile binned by Gender",
+       x = "Relative Position", y = "Citation Percentile") +
+  theme_minimal()
+gender_combine_p
 
 
 
